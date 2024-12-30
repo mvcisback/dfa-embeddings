9e16c71d1f280f5d2d5518302c124cca23f73474
Marcell Vazquez-Chanlatte <mvc@linux.com>
2024-12-30 18:55:49 +0000

    start working on Encoder.

--- src/dfa_embeddings/model.py
+++ src/dfa_embeddings/model.py
@@ -1,13 +1,93 @@
+import itertools
+
 import equinox as eqx
-from jaxtyping import Float, Array, PRNGKeyArray
+import jax
+import jax.numpy as jnp
+import numpy as np
+from dfa import DFA
+from jaxtyping import Array, PRNGKeyArray
+from jaxtyping import Bool, UInt64, Float
 from gatv2_eqx import GATv2
+
+
+def dfa2mat(dfa: DFA):
+    """Translates DFA into an adjacency matrix and feature vec.
+
+    TODO: Document.
+    """
+    assert len(dfa.inputs) < 64, "Currently, support at most 63 inputs."""
+
+    states = list(dfa.states())
+    tokens = sorted(dfa.inputs)
+    transitions = itertools.product(states, tokens)
+    transitions = [(s, t) for s, t in transitions if s != dfa._transition(s, t)]
+
+    m = len(states)
+    n = m + len(transitions)
+    adj = np.zeros((n, n), dtype=np.bool_)
+    features = np.zeros(n, dtype=np.uint64)
+
+    state2idx = {s: i for i, s in enumerate(states)}
+    token2idx = {t: i for i, t in enumerate(tokens)}
+    for s in states:
+        features[state2idx[s]] = int(dfa._label(s))
+
+    for i12, (s1, t) in enumerate(transitions, start=m):
+        s2 = dfa._transition(s1, t)
+        i1, i2 = map(state2idx.get, (s1, s2))
+
+        features[i12] |= np.uint64(1 << (token2idx[t] + 1))
+        adj[i2, i12] = adj[i12, i1] = True  # s2 -> (s1, t) -> s1.
+
+    return adj, features
 
 
 class DFAEncoder(eqx.Module):
     gnn: GATv2
-    node_encoder: eqx.nn.Linear
-    decoder: eqx.nn.Linear
-    node_tags: eqx.nn.Embedding
+    tags: eqx.nn.Embedding
+    n_tokens: int
 
-    def __init__(self, key: PRNGKeyArray):
-        ...
+    def __init__(self,
+                 n_tokens: int,
+                 hidden_dim: int | None = None,
+                 *, key: PRNGKeyArray):
+        self.n_tokens = n_tokens
+
+        if hidden_dim is None:
+            hidden_dim = n_tokens
+
+        key_gnn, key_tag = jax.random.split(key, 2)
+
+        self.gnn = GATv2(hidden_dim, key=key_gnn)
+        self.tags = eqx.nn.Embedding(num_embeddings=n_tokens+2,
+                                     embedding_size=hidden_dim,
+                                     key=key) 
+
+    def pack(self, dfa: DFA) -> tuple[Bool[Array, "n n"], UInt64[Array, "n"]]:
+        adj, features = dfa2mat(dfa)
+        return jnp.array(adj), jnp.array(features)
+
+    @jax.vmap
+    def unpack_and_tag(x) -> Float[Array, "d"]:
+        """Unpack bits and map to sum of feature attributes (tags)."""
+        indices = jnp.arange(1 + self.n_tokens)  # accepting bit + token bits.
+        return jax.vmap(lambda i: ((x >> i) & 1) * self.tags(i))(indices).sum()
+
+    def encode_packed_dfa(self,
+                          adj: Bool[Array, "n n"],
+                          nodes: UInt64[Array, "n"],
+                          n_iters: int,
+                          *, key: PRNGKeyArray) -> Float[Array, "n d"]:
+        x = unpack_and_tag(nodes)
+        return self.gnn(nodes=x, adj_mat=nodes, n_iters=n_iters, key=key)
+
+    def __call__(self, dfa: DFA,
+                 n_iters: int | None = None,
+                 *, key: PRNGKeyArray) -> Float[Array, "n d"]:
+        adj, nodes = self.pack(dfa)
+
+        if n_iters is None:
+            n_iters = nodes.shape[0]
+
+        return self.encode_packed_dfa(adj, nodes, n_iters, key=key)
+
