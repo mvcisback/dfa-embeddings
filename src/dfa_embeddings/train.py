762bbf3639824705d5fe48dd8417809a17f442ec
Marcell Vazquez-Chanlatte <mvc@linux.com>
2025-01-02 05:27:26 +0000

    Add prob_sat to loss.

--- src/dfa_embeddings/train.py
+++ src/dfa_embeddings/train.py
@@ -1,3 +1,4 @@
+import einops
 import equinox as eqx
 import funcy as fn
 import jax
@@ -16,15 +17,18 @@ oo = float('inf')
 
 
 @eqx.filter_jit
-def eval_model(model, adj, nodes, dists, *, key):
-    encoder, decoder = model
+def eval_model(model, adj, nodes, dists, psats, *, key):
+    encoder, dist_decoder, psat_decoder = model
     n_iters = 2 * nodes.shape[0]
     x = encoder.encode_packed_dfa(adj, nodes, n_iters, key=key)
-    out = jax.vmap(decoder)(x)
-    return ((out - dists)**2).mean()
+    out = jax.vmap(dist_decoder)(x)
+    l_dist = ((out - dists)**2 * (dists < 0)).mean()
+    out = jax.vmap(psat_decoder)(x)
+    l_psat = ((out - psats)**2).mean()
+    return l_dist + l_psat
 
 
-def distances_from_accepting(adj, nodes, n):
+def distances_from_accepting(adj, nodes):
     adj, nodes = map(np.asarray, (adj, nodes))
     g = nx.DiGraph(adj)
     # TODO: assert that accepting is a sink and only a single state.
@@ -35,27 +39,43 @@ def distances_from_accepting(adj, nodes, n):
     return dists
 
 
+def prob_reach_accepting(adj, nodes):
+    H = int(((nodes >> 1) != 0).sum())
+    row_sums = einops.reduce(adj.T, "row col -> row", "sum")
+    accept_idx = np.argwhere(nodes == 1.0)[0][0]
+    P_1 = adj.T / row_sums[:, None]
+    P_H = jnp.linalg.matrix_power(P_1, 2 * H)
+    return P_H[:, accept_idx]
+
+
+def gen_problems(encoder, n_tokens):
+    for dfa in rad_dfas(n_tokens=n_tokens):
+        adj, nodes = encoder.pack(dfa)
+        dists = distances_from_accepting(adj, nodes)
+        psats = prob_reach_accepting(adj, nodes)
+        yield adj, nodes, dists, psats
+
 
 def train(n_tokens=10, dim: int | None = 8, seed=0):
     # Hyperparameters
     LEARNING_RATE = 1e-3
 
     key = jax.random.PRNGKey(seed)
-    dfas = rad_dfas(n_tokens=n_tokens)
 
     init_key, call_key, decoder_key = jax.random.split(key, 3)
 
     encoder = DFAEncoder(n_tokens=n_tokens,
                          dim=dim,
                          key=init_key)
-    decoder = eqx.nn.MLP(dim, 1, 5, 3, key=decoder_key)
-    model = (encoder, decoder)
+    dist_decoder = eqx.nn.MLP(dim, 1, 5, 3, key=decoder_key)
+    psat_decoder = eqx.nn.MLP(dim, 1, 5, 3, key=decoder_key)
+    model = (encoder, dist_decoder, psat_decoder)
 
     optim = optax.adamw(LEARNING_RATE)
     opt_state = optim.init(eqx.filter(model, eqx.is_array))
 
-    test = fn.take(100, dfas)
-    dfa = test[0]
+    problems = gen_problems(encoder, n_tokens=n_tokens)
+    test = fn.take(100, problems)
 
     @eqx.filter_jit
     def make_step(
@@ -64,10 +84,11 @@ def train(n_tokens=10, dim: int | None = 8, seed=0):
         adj,
         nodes,
         dists,
+        psats,
         *,
         key
     ):
-        loss_value, grads = eqx.filter_value_and_grad(eval_model)(model, adj, nodes, dists, key=key)
+        loss_value, grads = eqx.filter_value_and_grad(eval_model)(model, adj, nodes, dists, psats, key=key)
         updates, opt_state = optim.update(
             grads, opt_state, eqx.filter(model, eqx.is_array)
         )
@@ -79,23 +100,18 @@ def train(n_tokens=10, dim: int | None = 8, seed=0):
     while True:
         epoch += 1
         print(f'====== epoch {epoch} ======')
-        batch = fn.take(1000, dfas)
-        for i in range(10):
+        batch = fn.take(10, problems)
+        for i in range(1_000):
             loss = 0.0
             print(f"--- {i}")
-            for dfa in batch:
-                adj, nodes = encoder.pack(dfa)
-                dists = distances_from_accepting(adj, nodes, n=len(dfa.states()))
-
-                model, opt_state, train_loss = make_step(model, opt_state, adj, nodes, dists, key=call_key)
+            for adj, nodes, dists, psats in batch:
+                model, opt_state, train_loss = make_step(model, opt_state, adj, nodes, dists, psats, key=call_key)
                 loss += train_loss
-            print(f'training: {loss / 1000}')
+            print(f'training: {loss / len(batch)}')
 
             loss = 0.0
-            for dfa in test:
-                adj, nodes = encoder.pack(dfa)
-                dists = distances_from_accepting(adj, nodes, n=len(dfa.states()))
-                loss += eval_model(model, adj, nodes, dists=dists, key=call_key)
+            for adj, nodes, dists, psats in test:
+                loss += eval_model(model, adj, nodes, dists=dists, psats=psats, key=call_key)
             print(f'test: {loss / len(test)}')
             if loss < best:
                 best = loss
